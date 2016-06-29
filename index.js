9d315bfef112fbb85be785f7226ef6346c790ad3
luochenxun <luochenxun@gmail.com>
2016-06-29 11:44:09 +0800

    rm app.js

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+/*******  import module  **********/
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
 
+/*******  env config  **********/
+// forbit the server info leak
+app.disable('x-powered-by');
+
+// db
 mongoose.connect('mongodb://120.25.234.188:27017/timebottles_test');
 var db = mongoose.connection;
 var app = express();
 var port = 8081;
 
-app.get('/', function(req, res) {
-    res.send('Welcome to time bottles.');
-})
-
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
     extended: true
 })); // for parsing application/x-www-form-urlencoded
+
+/*******  router  **********/
+app.get('/', function(req, res) {
+    res.send('Welcome to time bottles.');
+})
+
 // app.use(multer()); // for parsing multipart/form-data
 app.use('/oauth2', require('./rest/oauth2'));
 app.use('/users', require('./rest/user.resource'));
 
+/*******  start server & fault-tolerance  **********/
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', (cb) => {
     console.log('mongo connected.');
