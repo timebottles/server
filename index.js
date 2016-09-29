64e5754e8d623a4d829248e4314c89a9367cf06e
luochenxun <chenxun.luo@jyblife.com>
2016-09-30 00:21:02 +0800

    restart the proj

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
 /*******  import module  **********/
 var express = require('express');
+var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 
-/*******  env config  **********/
+
+/*******   配置   **********/
+// // db
+// mongoose.connect('mongodb://120.25.234.188:27017/timebottles_test');
+// var db = mongoose.connection;
+
+// 设置全局变量
+app.set('port', process.env.PORT || 8081);
+
+// logger
+app.use(logger('dev'));
+
 // forbit the server info leak
 app.disable('x-powered-by');
 
-// db
-mongoose.connect('mongodb://120.25.234.188:27017/timebottles_test');
-var db = mongoose.connection;
-var app = express();
-var port = 8081;
-
+//TODO: uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+// Static parse
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 /*******  router  **********/
-app.get('/', function(req, res) {
-    res.send('Welcome to time bottles.');
-})
+// 设置路由 - 首页
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.send('欢迎来到时光瓶');
+});
 
 // app.use(multer()); // for parsing multipart/form-data
 app.use('/oauth2', require('./rest/oauth2'));
 app.use('/users', require('./rest/user.resource'));
 
-/*******  start server & fault-tolerance  **********/
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', (cb) => {
-    console.log('mongo connected.');
-    var server = app.listen(port, function() {
-        var host = server.address().address;
-        console.log("Server listening at http://%s : %s", host, port);
-    });
+// Error process
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+// 开启服务，监听配置中给定的端口
+app.listen(port, function() {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+// /*******  start server & fault-tolerance  **********/
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', (cb) => {
+//   console.log('mongo connected.');
+//   var server = app.listen(port, function() {
+//     var host = server.address().address;
+//     console.log("Server listening at http://%s : %s", host, port);
+//   });
+// });
